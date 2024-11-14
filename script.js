475fa2804de82f6093c703f5137dcd3e2c627eb3
anusha-soh <anushasohail3@gmail.com>
2024-11-14 00:42:58 -0800

    basic CV builder is createdm

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// const form = document.getElementById('resume-form') as HTMLFormElement;
-// const resumeDisplay = document.getElementById('generated-resume') as HTMLDivElement;
-// form.addEventListener( 'submit', (event : Event) => {
-//     event.preventDefault();
-//     const name = (document.getElementById('name') as HTMLInputElement).value
-//     const profession = (document.getElementById('profession') as HTMLInputElement).value
-//     const objective = (document.getElementById('objective') as HTMLInputElement).value
-//     const experience = (document.getElementById('experience') as HTMLInputElement).value
-//     const education = (document.getElementById('education') as HTMLInputElement).value
-//     const skills = (document.getElementById('skills') as HTMLInputElement).value
-//     const email = (document.getElementById('email') as HTMLInputElement).value
-//     const tel = (document.getElementById('tel') as HTMLInputElement).value
-//     const linkedin = (document.getElementById('linkedin') as HTMLInputElement).value
-//     const address = (document.getElementById('address') as HTMLInputElement).value
-//     const resume = `
-//     <ul>
-//         <li>${name}</li>
-//         <li>${profession}</li>
-//         <li>${objective}</li>
-//         <li>${experience}</li>
-//         <li>${education}</li>
-//         <li>${skills}</li>
-//         <li>${email}</li>
-//         <li>${tel}</li>
-//         <li>${linkedin}</li>
-//         <li>${address}</li>
-//     </ul>
-//     `;
-//     if (resumeDisplay) { 
-//         resumeDisplay.innerHTML = resume;
-//     } else { 
-//         console.error(`missing field`)
-//     }
-// })
 var form = document.getElementById('resume-form');
 var resumeDisplay = document.getElementById('generated-resume');
 form.addEventListener('submit', function (event) {
@@ -46,7 +12,7 @@ form.addEventListener('submit', function (event) {
     var tel = document.getElementById('tel').value;
     var linkedin = document.getElementById('linkedin').value;
     var address = document.getElementById('address').value;
-    var resume = "\n        <ul>\n            <li><strong>Name:</strong> ".concat(name, "</li>\n            <li><strong>Profession:</strong> ").concat(profession, "</li>\n            <li><strong>Objective:</strong> ").concat(objective, "</li>\n            <li><strong>Experience:</strong> ").concat(experience, "</li>\n            <li><strong>Education:</strong> ").concat(education, "</li>\n            <li><strong>Skills:</strong> ").concat(skills, "</li>\n            <li><strong>Email:</strong> ").concat(email, "</li>\n            <li><strong>Phone:</strong> ").concat(tel, "</li>\n            <li><strong>LinkedIn:</strong> ").concat(linkedin, "</li>\n            <li><strong>Address:</strong> ").concat(address, "</li>\n        </ul>\n    ");
+    var resume = "\n\n\n\n    <main class=\"res-main\">\n    <header>\n        <h1 class=\"resh1\">".concat(name, "</h1>\n        <h2 class=\"resh2\">").concat(profession, "</h2>\n    </header>\n    \n    \n    <h3 class=\"resh3\">Objective</h3>\n    <p class=\"obj\">").concat(objective, "</p>\n    \n    <h3 class=\"resh3\">Experience</h3>\n    <div class=\"exp-item\">\n        <!-- <p class=\"exp-date\">JAN 20XX - AUG 20XX</p> -->\n        <p>").concat(experience, "</p>\n    </div>\n    <!-- <div class=\"exp-item\">\n        <p class=\"experience-date\">OCT 20XX - MAY 20XX</p>\n        <p>Teacher's Aide | Balsam Elementary School | Santa Fe, NM</p>\n    </div>\n    <div class=\"exp-item\">\n        <p class=\"experience-date\">SEP 20XX - JUN 20XX</p>\n        <p>Teacher's Aide | Balsam Elementary School | Santa Fe, NM</p>\n    </div>\n    <p>Key responsibilities: planning and delivering effective instruction across various subjects and grade levels, assessing and monitoring student progress, and providing individualized support and intervention as needed.</p> -->\n    \n    <h3 class=\"resh3\">Education</h3>\n    <p>").concat(education, "</p>\n    \n    <h3 class=\"resh3\">Skills</h3>\n    <p>").concat(skills, "</p>\n    \n    <!-- <h3>Leadership</h3>\n    <p>As a teacher, I maintain a positive classroom environment that promotes student engagement, behavior management, and social-emotional development.</p>\n    <ul>\n        <li></li>\n        <li></li>\n        <li></li>\n        <li></li>\n    </ul> -->\n    <!-- <h3>References</h3>\n    <p>Available upon request.</p>\n     -->\n    <div class=\"contact\">\n        <h3 class=\"resh3\">Contact Information</h3>\n        <p>Address: ").concat(address, "</p>\n        <p>Phone: ").concat(tel, "</p>\n        <p>Email: ").concat(email, "</p>\n        <p>linkedin: ").concat(linkedin, "</p>\n    </div>\n</main>\n        \n\n\n\n    ");
     if (resumeDisplay) {
         resumeDisplay.innerHTML = resume;
     }
--- script.ts
+++ script.ts
@@ -1,46 +1,3 @@
-
-// const form = document.getElementById('resume-form') as HTMLFormElement;
-// const resumeDisplay = document.getElementById('generated-resume') as HTMLDivElement;
-
-// form.addEventListener( 'submit', (event : Event) => {
-//     event.preventDefault();
-
-//     const name = (document.getElementById('name') as HTMLInputElement).value
-//     const profession = (document.getElementById('profession') as HTMLInputElement).value
-//     const objective = (document.getElementById('objective') as HTMLInputElement).value
-//     const experience = (document.getElementById('experience') as HTMLInputElement).value
-//     const education = (document.getElementById('education') as HTMLInputElement).value
-//     const skills = (document.getElementById('skills') as HTMLInputElement).value
-//     const email = (document.getElementById('email') as HTMLInputElement).value
-//     const tel = (document.getElementById('tel') as HTMLInputElement).value
-//     const linkedin = (document.getElementById('linkedin') as HTMLInputElement).value
-//     const address = (document.getElementById('address') as HTMLInputElement).value
-
-//     const resume = `
-//     <ul>
-//         <li>${name}</li>
-//         <li>${profession}</li>
-//         <li>${objective}</li>
-//         <li>${experience}</li>
-//         <li>${education}</li>
-//         <li>${skills}</li>
-//         <li>${email}</li>
-//         <li>${tel}</li>
-//         <li>${linkedin}</li>
-//         <li>${address}</li>
-//     </ul>
-//     `;
-
-//     if (resumeDisplay) { 
-//         resumeDisplay.innerHTML = resume;
-//     } else { 
-//         console.error(`missing field`)
-//     }
-
-
-
-// })
-
 const form = document.getElementById('resume-form') as HTMLFormElement;
 const resumeDisplay = document.getElementById('generated-resume') as HTMLDivElement;
 
@@ -59,18 +16,13 @@ form.addEventListener('submit', (event: Event) => {
     const address = (document.getElementById('address') as HTMLInputElement).value;
 
     const resume = `
-        <ul>
-            <li><strong>Name:</strong> ${name}</li>
-            <li><strong>Profession:</strong> ${profession}</li>
-            <li><strong>Objective:</strong> ${objective}</li>
-            <li><strong>Experience:</strong> ${experience}</li>
-            <li><strong>Education:</strong> ${education}</li>
-            <li><strong>Skills:</strong> ${skills}</li>
-            <li><strong>Email:</strong> ${email}</li>
-            <li><strong>Phone:</strong> ${tel}</li>
-            <li><strong>LinkedIn:</strong> ${linkedin}</li>
-            <li><strong>Address:</strong> ${address}</li>
-        </ul>
+
+
+
+            
+
+
+
     `;
 
     if (resumeDisplay) { 
